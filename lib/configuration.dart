993b973ba17078fcc38ce7d6a334b869948dda13
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-05 23:16:24 +0530

    config color

---
+++ lib/configuration.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Color primaryGreen = Color(0xff416d6d);
+List<BoxShadow> shodowList = [
+  BoxShadow(color: Colors.grey, blurRadius: 30, offset: Offset(0, 10))
+];
+
+List<Map> categoris = [
+  {'name': 'Cats', 'iconPath': 'images/cat.png'},
+  {'name': 'Dogs', 'iconPath': 'images/dog.png'},
+  {'name': 'Bunnies', 'iconPath': 'images/rabbit.png'},
+  {'name': 'Parrots', 'iconPath': 'images/parrot.png'},
+  {'name': 'Horse', 'iconPath': 'images/horse.png'},
+];
