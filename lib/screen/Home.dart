e8e21d446ce16c32b6f4152418bb97b33972e650
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-05 23:55:00 +0530

    animation container

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -9,9 +9,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double xOffset = 0;
+  double yOffset = 0;
+  double scaleFactor = 1;
+
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
+      transform: Matrix4.translationValues(xOffset, yOffset, 0)
+        ..scale(scaleFactor),
       duration: Duration(milliseconds: 250),
       color: Colors.white,
       child: Column(
@@ -19,25 +25,36 @@ class _HomeState extends State<Home> {
           SizedBox(
             height: 50,
           ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
-            ),
-            Column(
-              children: [
-                Text('Location'),
-                Row(children: [
-                  Icon(
-                    Icons.location_on,
-                    color: primaryGreen,
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {
+                      setState(() {
+                        xOffset = 200;
+                        yOffset = 100;
+                        scaleFactor = .4;
+                      });
+                    },
                   ),
-                  Text('Ukrain')
-                ])
-              ],
-            ),
-            CircleAvatar()
-          ])
+                  Column(
+                    children: [
+                      Text('Location'),
+                      Row(children: [
+                        Icon(
+                          Icons.location_on,
+                          color: primaryGreen,
+                        ),
+                        Text('India')
+                      ])
+                    ],
+                  ),
+                  CircleAvatar()
+                ]),
+          )
         ],
       ),
     );
