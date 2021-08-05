ba6222e2bdc1c72784fe892c748c6434591462fe
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-06 00:28:29 +0530

    slider done

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -13,13 +13,15 @@ class _HomeState extends State<Home> {
   double yOffset = 0;
   double scaleFactor = 1;
 
+  bool isDrawerOpen = false;
+
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       transform: Matrix4.translationValues(xOffset, yOffset, 0)
         ..scale(scaleFactor),
       duration: Duration(milliseconds: 250),
-      color: Colors.white,
+      color: Colors.grey[200],
       child: Column(
         children: [
           SizedBox(
@@ -30,16 +32,29 @@ class _HomeState extends State<Home> {
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  IconButton(
-                    icon: Icon(Icons.menu),
-                    onPressed: () {
-                      setState(() {
-                        xOffset = 200;
-                        yOffset = 100;
-                        scaleFactor = .4;
-                      });
-                    },
-                  ),
+                  isDrawerOpen
+                      ? IconButton(
+                          icon: Icon(Icons.arrow_back_ios),
+                          onPressed: () {
+                            setState(() {
+                              xOffset = 0;
+                              yOffset = 0;
+                              scaleFactor = 1;
+                              isDrawerOpen = false;
+                            });
+                          },
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () {
+                            setState(() {
+                              xOffset = 210;
+                              yOffset = 150;
+                              scaleFactor = .6;
+                              isDrawerOpen = true;
+                            });
+                          },
+                        ),
                   Column(
                     children: [
                       Text('Location'),
@@ -54,7 +69,57 @@ class _HomeState extends State<Home> {
                   ),
                   CircleAvatar()
                 ]),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.search),
+                  Text('Search pet to adopt'),
+                  Icon(Icons.settings)
+                ],
+              )),
+          Container(
+            height: 120,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categoris.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          margin: EdgeInsets.only(left: 20, top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: shodowList,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          // height: 60,
+                          // width: 80,
+                          child: Image.asset(
+                            categoris[index]['iconPath'],
+                            height: 50,
+                            width: 50,
+                          ),
+                        ),
+                        Text(categoris[index]['name'])
+                      ],
+                    ),
+                  );
+                }),
+          ),
         ],
       ),
     );
